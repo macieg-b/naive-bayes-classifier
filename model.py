be309d452b275bda68b4a4ff1e3942b917189a2e
macieg-b <bartlomiejczyk.maciej@gmail.com>
2017-10-22 17:28:10 +0200

    1. Fix method to fit parameters 2. Add method to classify probe 3. Add method to test if classification is proper 4. Fix method to extract data

--- model.py
+++ model.py
@@ -1,28 +1,46 @@
 import random
+import numpy as np
 
 
 class NaiveBayes:
     __decisions_distribution = dict()
-    __conditional_distribution = ""
+    __conditional_distribution = dict()
 
     def __init__(self):
         pass
 
-    def fit(self, xu, yu):
-        animal_classes = dict()
-        for animal_class in set(yu):
-            animal_classes[animal_class] = 0
-
-        for i in range(0, len(xu)):
-            animal_classes[yu[i]] += 1
-
-        for animal_class in set(yu):
-            self.__decisions_distribution[animal_class] = float(animal_classes[animal_class]) / len(yu)
-
+    def fit(self, data, attributes):
+        for probe in data:
+            key = probe[len(probe) - 1]
+            try:
+                self.__decisions_distribution[key] += 1
+            except KeyError:
+                self.__decisions_distribution[key] = 1
+            for i in range(0, len(attributes)):
+                if probe[i] == "true":
+                    try:
+                        self.__conditional_distribution[key, attributes[i]] += 1
+                    except KeyError:
+                        self.__conditional_distribution[key, attributes[i]] = 1
+        for animal_class in self.__decisions_distribution:
+            self.__decisions_distribution[animal_class] /= float(len(data))
+        for item in self.__conditional_distribution:
+            self.__conditional_distribution[item] /= float(len(data))
         pass
 
-    def predict_probe(self, x):
-        return
+    def predict_probe(self, probe, attributes):
+        final_conditional_probability = dict()
+        for animal_class in self.__decisions_distribution:
+            attributes_probability = []
+            for i in range(0, len(attributes)):
+                if probe[i] == "true":
+                    try:
+                        attributes_probability.append(self.__conditional_distribution[animal_class, attributes[i]])
+                    except KeyError:
+                        attributes_probability.append(0)
+
+            final_conditional_probability[animal_class] = np.prod(np.array(attributes_probability)) * self.__decisions_distribution[animal_class]
+        return max(final_conditional_probability, key=final_conditional_probability.get)
 
 
 class BayesUtil:
@@ -30,7 +48,7 @@ class BayesUtil:
         pass
 
     @staticmethod
-    def remove_unnecessary(data, indexes):
+    def remove_unnecessary_data(data, indexes):
         for row in data:
             i = 0
             for index in indexes:
@@ -39,20 +57,25 @@ class BayesUtil:
         pass
 
     @staticmethod
+    def remove_unnecessary_attributes(attributes, indexes):
+        i = 0
+        for index in indexes:
+            del attributes[index - i]
+            i = i + 1
+        for i in range(0, len(attributes)):
+            attributes[i] = attributes[i][0]
+
+    pass
+
+    @staticmethod
     def divide_data(data):
         random.shuffle(data)
         teach_size = len(data) * 2 / 3
         return data[:teach_size], data[teach_size:]
 
     @staticmethod
-    def split_data_and_classes(data):
-        xu = []
-        yu = []
-        for row in data:
-            xu.append(row[:len(row) - 1])
-            yu.append(row[len(row) - 1])
-        return xu, yu
-
-    @staticmethod
-    def bayes_error(y, pred_y):
-        pass
+    def is_classified_properly(probe, selected_class):
+        correct = False
+        if probe[len(probe)-1] == selected_class:
+            correct = True
+        return correct
--- run.py
+++ run.py
@@ -6,10 +6,14 @@ FILE_PATH = "data/zoo.arff"
 
 data_set = arff.load(open(FILE_PATH, 'rb'))
 data = data_set['data']
+attributes = data_set['attributes']
 
-BayesUtil.remove_unnecessary(data, [0, 13])
+BayesUtil.remove_unnecessary_data(data, [0, 13])
+BayesUtil.remove_unnecessary_attributes(attributes, [0, 13, 14])
 teach_data, test_data = BayesUtil.divide_data(data)
 
-xu, yu = BayesUtil.split_data_and_classes(teach_data)
 naive_bayes = NaiveBayes()
-naive_bayes.fit(xu, yu)
+naive_bayes.fit(data, attributes)
+
+selected_class = naive_bayes.predict_probe(test_data[0], attributes)
+print "Is classified properly: " + str(BayesUtil.is_classified_properly(test_data[0], selected_class))
